43cedef267b067407e7cdb9aeb7c362ed44c9d71
Galirus_Official™ <114099161+Galirus404@users.noreply.github.com>
2022-10-30 11:40:23 +0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,7 @@
+# BFB
+>>>>>>>>>> SUBSCRIBE <<<<<<<<<
+
+# channel saya biar Saya bisa semangat membuat script baru :)
+
+# Jika ada pertanyaan silahkan hubungi 085732779537 (WhatsApp)
+
---
+++ bfb.sh
@@ -0,0 +1,82 @@
+#bin#bash#Jangan DiUbah #Hargai Pembuat
+###---[ WARNA ]--###
+P = '\033[97m'  # PUTIH
+M = '\033[91m' # MERAH
+hh = '\033[92m'  # HIJAU
+kk = '\033[93m'  # KUNING
+clear
+								
+sleep 2
+echo "Menginstall Bahan Yang Dibutuhkan " | lolcat
+sleep 3
+apt update -y
+apt upgrade -y
+apt install ruby -y
+gem install lolcat -y
+apt install python python2 -y
+apt install toilet -y
+apt install bash
+pip install requests mechanize bs4 rich random-user-agent
+clear
+								
+clear
+sleep 3
+								
+toilet --g "SUBREK" | lolcat
+                                                 		
+echo "DULU DONG CHANNEL SAYA BIAR TAMBAH SEMANGAT :)" | lolcat
+sleep 3
+xdg-open "https://youtube.com/channel/UCOJ93mxZHetWOzsARhHbLDQ"
+sleep 10
+clear
+#÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷
+								
+
+while [ $else = $ulang ]
+do
+clear
+
+toilet "Wellcome" | lolcat
+date | lolcat
+
+								
+#÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷÷
+
+
+        read -p  "$M Username :" siapa
+if [ $siapa = "Galirus" ]
+then
+
+        read -p  "$P Password :" apa
+
+if [ $apa = "404" ]
+then
+
+
+clear
+echo "Selamat Username & Password DiTerima...!!!" | lolcat
+sleep 5
+clear
+cd
+cd BFB
+chmod 777 1.py
+python 1.py
+
+
+
+else
+clear
+echo "Password Yang Anda Masukkan Salah " | lolcat
+sleep 3
+xdg-open "https://api.whatsapp.com/send?phone=6285732779537&text=Assalamualaikum%20kak%20Username%20Dan%20Password%20apa"
+fi
+else
+clear
+echo "Username Yang Anda Masukkan Salah " | lolcat
+sleep 3
+
+xdg-open "https://api.whatsapp.com/send?phone=6285732779537&text=Assalamualaikum%20kak%20Username%20Dan%20Password%20apa"
+
+fi
+done
+								
